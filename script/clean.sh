0913c83ebea019d86a3cb35a90d68eb2875ca094
@slovacus <slovacus@gmail.com>
2016-12-14 06:52:27 -0500

    [#12] Fix: script of makefile

--- script/clean.sh
+++ script/clean.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# shellcheck disable=SC1091
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 {
-    rm -rf **/**/*__pycache__
-    rm -rf **/**/*.pyc
-    rm -rf **/**/*.swp
+    rm -rf -- **/**/*__pycache__
+    rm -rf -- **/**/*.pyc
+    rm -rf -- **/**/*.swp
 } >> /dev/null 2>&1
--- script/grip.sh
+++ script/grip.sh
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # shellcheck source=script/bootstrap.sh
+# shellcheck disable=SC1091
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 grip --pass "${GITHUB_API_TOKEN}" ${GRIP_PORT}
