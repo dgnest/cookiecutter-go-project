6b940dcf585c79c7ac9a6029d9ce4715b7c6a1fd
@slovacus <slovacus@gmail.com>
2017-03-07 03:44:05 -0500

    !#13 Fix: clean gvm

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,13 +5,6 @@ export PROJECT_NAME=cookiecutter-go-project
 export PYTHON_VERSION=2.7.9
 export PYENV_NAME="${PROJECT_NAME}"
 
-export GVM_NAME="${PROJECT_NAME}"
-export GVM_PATHS_NAME=(
-    "src"
-    "pkg"
-    "bin"
-)
-
 export GRIP_PORT=6430
 
 # Vars Dir
